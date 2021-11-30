9f0bdfe9ff253015e06ee7bc3de1d03705ec030d
Kartavya Vashishtha <sendtokartavya@gmail.com>
2021-11-30 11:53:25 +0530

    Styling changes And more reactivity

--- www/index.js
+++ www/index.js
@@ -1,14 +1,30 @@
 import * as wasm from "wasm-easy-crypt";
 
-document.getElementById("do_it_button").onclick = () => {
-	let en = document.getElementById("encrypt");
-	let de = document.getElementById("decrypt");
-	let key = document.getElementById("key");
-	if (en.value != "") {
-		de.value = wasm.encrypt (en.value, key.value);
-	} else if (de.value != "") {
-		en.value = wasm.decrypt (de.value, key.value);
+let pl = document.getElementById("plaintext");
+let cp = document.getElementById("ciphertext");
+let key = document.getElementById("key");
+let button = document.getElementById("do_it_button");
+
+button.onclick = () => {
+	if (pl.value != "") {
+		cp.value = wasm.encrypt (pl.value, key.value);
+	} else if (cp.value != "") {
+		pl.value = wasm.decrypt (cp.value, key.value);
 	} else {
-		alert("Both Encrypt and decrypt fields are blank!");
+		alert("Both Plaintext and Ciphertext fields are blank!");
 	}
+}
+
+pl.oninput = () => {
+	//cp.disabled = (pl.value != "");
+	cp.value = "";
+	button.value = (pl.value == "" && cp.value == "") ? "Convert" : "Encrypt";
+	button.disabled = (pl.value == "" && cp.value == "");
+}
+
+cp.oninput = () => {
+	//pl.disabled = (cp.value != "");
+	pl.value = "";
+	button.value = (pl.value == "" && cp.value == "") ? "Convert" : "Decrypt";
+	button.disabled = (pl.value == "" && cp.value == "");
 }
